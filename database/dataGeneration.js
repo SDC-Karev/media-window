04010c429c9f3ef33e8082a0e3b0d50cab107137
Ryan Zigler <rpzigler@gmail.com>
2020-08-29 15:19:56 -0500

    updated generation script to format date for PostgreSQL

--- database/dataGeneration.js
+++ database/dataGeneration.js
@@ -41,6 +41,42 @@ const generateTags = () => {
   return tags;
 };
 
+const generateDate = () => {
+  // Thu Sep 29 2011 12:39:32 GMT-0500 (Central Daylight Time)
+  const date = faker.date.past(10);
+  const dateString = date.toString();
+  const splitDate = dateString.split(' ');
+  const year = splitDate[3];
+  const day = splitDate[2];
+  const month = splitDate[1];
+  let monthNum = '';
+
+  const monthAbbr = {
+    Jan: '01',
+    Feb: '02',
+    Mar: '03',
+    Apr: '04',
+    May: '05',
+    Jun: '06',
+    Jul: '07',
+    Aug: '08',
+    Sep: '09',
+    Oct: '10',
+    Nov: '11',
+    Dec: '12',
+  };
+
+  Object.entries(monthAbbr).forEach(([key, value]) => {
+    if (month === key) {
+      monthNum = value;
+    }
+    return monthNum;
+  });
+
+  const formattedDate = `${year}-${monthNum}-${day}`;
+  return formattedDate;
+};
+
 const reviews = [
   'Overhwelmingly Positive',
   'Very Positive',
@@ -72,7 +108,7 @@ function write(callback) {
     const description = faker.lorem.paragraph();
     const recentReviews = faker.random.arrayElement(reviews);
     const allReviews = faker.random.arrayElement(reviews);
-    const releaseDate = faker.date.past(10);
+    const releaseDate = generateDate();
     const developer = `${faker.hacker.verb()} ${faker.hacker.noun()}`;
     const publisher = `${faker.hacker.verb()} ${faker.hacker.noun()}`;
     const primaryRecord = `"${images}","${videos}","${tags}",${gameTitle},${description},${recentReviews},${allReviews},${releaseDate},${developer},${publisher}\n`;
