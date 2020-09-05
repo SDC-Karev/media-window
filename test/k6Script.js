aebbef8946a62758a06a21ed8f21b2d6863e63bd
Ryan Zigler <rpzigler@gmail.com>
2020-09-05 16:47:06 -0500

    updated k6 script

--- test/k6Script.js
+++ test/k6Script.js
@@ -16,19 +16,13 @@ export let options = {
     'http_req_duration': ['p(95)<100']
   },
   stages: [
-    { duration: '10s', target: 100 }, // get to 100 RPS in 10s
-    { duration: '1m', target: 100 }, // hold 100 RPS for 1m
-    { duration: '10s', target: 500 }, // move to 500 RPS in 10s
-    { duration: '2m', target: 500 }, // hold 500 rps for 2m
-    { duration: '10s', target: 1000 }, // move to 1000 RPS in 10s
+    { duration: '10s', target: 100 }, // ramp to 100 RPS in 10s
+    { duration: '20s', target: 100 }, // hold 100 RPS for 20s
+    { duration: '30s', target: 1000 }, // ramp to 1000 RPS over 30s
     { duration: '8m', target: 1000 }, // hold 1000 RPS for 8m
-    { duration: '10s', target: 1500 }, // spike to 1500 briefly
-    { duration: '10s', target: 1000 },
-    { duration: '8m', target: 1000 }, // hold 1000 RPS for 8 min
-    { duration: '10s', target: 100 }, // scale down to 100 RPS in 10s
-    { duration: '30s', target: 100 }, // hold 100 RPS for 30s
-    { duration: '10s', target: 10 },
-    { duration: '10s', target: 0 }, // taper to 0 over 20s
+    { duration: '30s', target: 100 }, // taper to 100 RPS over 30s
+    { duration: '20s', target: 100 }, // hold 100 RPS for 20s
+    { duration: '10s', target: 0 }, // taper to 0 over 10s
   ],
 }
 
