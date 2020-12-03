a61593c79fc985a2c60de4a2806b95734ceacca9
Ryan Zigler <rpzigler@gmail.com>
2020-12-03 12:13:32 -0600

    Update postgresSeed.sql

--- database/postgresSeed.sql
+++ database/postgresSeed.sql
@@ -1,5 +1,5 @@
 DELETE FROM games;
 
 COPY games(images, videos, tags, name,description, all_reviews, recent_reviews,release_date, developer, publisher)
-  FROM '/home/ryan/Documents/hack-reactor/system-design-capstone/media-window/gameData.csv'
+  FROM 'FULL_PATH_TO_PROJECT_ROOT_DIR/media-window/gameData.csv' /* ex: 'home/ryan/Documents/media-window/gameData.csv' */
   DELIMITER ',' CSV HEADER;
